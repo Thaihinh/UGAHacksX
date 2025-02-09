0dfd1f760744352a5ebb6070ee34614d77cf8445
raycreatecode <raymondtan120@gmail.com>
2025-02-09 04:59:40 -0500

    uniformed the fonts

--- end screen/end_screen.gd
+++ end screen/end_screen.gd
@@ -2,7 +2,11 @@ extends Control
 
 @onready var score_label: Label = $ScoreLabel
 @onready var kills_label: Label = $KillsLabel
+@onready var high_score_label: Label = $HighScoreLabel
 
 func _ready() -> void:
 	score_label.text = "SCORE: " + str(Global.score)
 	kills_label.text = "KILLS: " + str(Global.kills)
+	if Global.score > Global.highscore:
+		Global.highscore = Global.score
+	high_score_label.text = "HIGH SCORE: " + str(Global.highscore)
--- persistent/global.gd
+++ persistent/global.gd
@@ -3,3 +3,4 @@ extends Node
 var health: int = 5
 var score: int = 0
 var kills: int = 0
+var highscore: int = 0
